484d6a4d9aaf93fbac2f001f8e0c6ad5b6daf0df
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-17 20:17:09 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,32 @@
-// Copyright 2025 UNN-CS Team
-
-#include <gtest/gtest.h>
-#include <cstdint>
 #include "circle.h"
+#include "tasks.h"
+#include <gtest/gtest.h>
 
+TEST(CircleTest, RadiusUpdatesCorrectly) {
+    Circle c(10);
+    ASSERT_DOUBLE_EQ(c.getRadius(), 10);
+    ASSERT_NEAR(c.getFerence(), 62.8319, 1e-4);
+    ASSERT_NEAR(c.getArea(), 314.159, 1e-3);
+}
 
+TEST(CircleTest, FerenceUpdatesCorrectly) {
+    Circle c(10);
+    c.setFerence(100);
+    ASSERT_NEAR(c.getRadius(), 100 / (2 * 3.14159), 1e-4);
+}
+
+TEST(CircleTest, AreaUpdatesCorrectly) {
+    Circle c(10);
+    c.setArea(100);
+    ASSERT_NEAR(c.getRadius(), sqrt(100 / 3.14159), 1e-4);
+}
+
+TEST(TaskTest, EarthGap) {
+    double gap = calculateGap();
+    ASSERT_NEAR(gap, 0.159, 1e-3);
+}
+
+TEST(TaskTest, PoolCost) {
+    double cost = calculatePoolCost();
+    ASSERT_NEAR(cost, 44000, 1);
+}
