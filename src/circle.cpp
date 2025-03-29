77c3be6319dd03c40b369352e615c81500e4cbcd
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-30 02:00:55 +0300

    Update circle.cpp

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,29 +1,48 @@
-// Copyright 2022 UNN-CS Team
+// Copyright 2024 New Developer
 #include "circle.h"
 #include <cmath>
 
-Circle::Circle(double r) {
-    setRadius(r);
+constexpr double PI = acos(-1.0); // Использована константа через acos
+
+Circle::Circle(double radius) {
+    setRadius(radius);
 }
 
-void Circle::setRadius(double r) {
-    radius = r;
-    ference = 2 * M_PI * r;
-    area = M_PI * r * r;
+void Circle::updateFromRadius() {
+    m_circumference = 2 * PI * m_radius;
+    m_area = PI * m_radius * m_radius;
 }
 
-void Circle::setFerence(double f) {
-    ference = f;
-    radius = f / (2 * M_PI);
-    area = M_PI * radius * radius;
+void Circle::updateFromCircumference() {
+    m_radius = m_circumference / (2 * PI);
+    m_area = PI * m_radius * m_radius;
 }
 
-void Circle::setArea(double a) {
-    area = a;
-    radius = std::sqrt(a / M_PI);
-    ference = 2 * M_PI * radius;
+void Circle::updateFromArea() {
+    m_radius = sqrt(m_area / PI);
+    m_circumference = 2 * PI * m_radius;
 }
 
-double Circle::getRadius() const { return radius; }
-double Circle::getFerence() const { return ference; }
-double Circle::getArea() const { return area; }
+// Дополнительные проверки в сеттерах
+void Circle::setRadius(double radius) {
+    if (radius < 0) throw std::invalid_argument("Invalid radius");
+    m_radius = radius;
+    updateFromRadius();
+}
+
+void Circle::setCircumference(double circumference) {
+    if (circumference < 0) throw std::invalid_argument("Invalid length");
+    m_circumference = circumference;
+    updateFromCircumference();
+}
+
+void Circle::setArea(double area) {
+    if (area < 0) throw std::invalid_argument("Invalid area");
+    m_area = area;
+    updateFromArea();
+}
+
+// Геттеры
+double Circle::getRadius() const { return m_radius; }
+double Circle::getCircumference() const { return m_circumference; }
+double Circle::getArea() const { return m_area; }
