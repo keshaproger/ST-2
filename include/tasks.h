d5be0abaa31e0d45ff1abfe22602023306b82c7f
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-30 00:50:34 +0300

    Delete include/tasks.h

--- include/tasks.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2025 Коньков И.
-
-#ifndef INCLUDE_TASKS_H_
-#define INCLUDE_TASKS_H_
-
-double calculateGap(double delta = 1.0);
-double calculatePoolCost(int value);
-
-#endif // INCLUDE_TASKS_H_
