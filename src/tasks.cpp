d6e7a1fe5386012e9801b9ae2ae46799b05266f9
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-30 01:17:28 +0300

    Update tasks.cpp

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -1,1 +1,24 @@
+// Copyright 2025 UNN-CS Team
+#include "tasks.h"
+#include "circle.h"
 
+double calculateRopeGap() {
+    const double earthRadius = 6378100.0; // В метрах
+    Circle earth(earthRadius);
+    earth.setFerence(earth.getFerence() + 1.0);
+    return earth.getRadius() - earthRadius;
+}
+
+double calculatePoolCost() {
+    const double poolRadius = 3.0;
+    const double width = 1.0;
+    const double outerRadius = poolRadius + width;
+
+    Circle pool(poolRadius);
+    Circle outer(outerRadius);
+
+    double concreteArea = outer.getArea() - pool.getArea();
+    double fenceLength = outer.getFerence();
+
+    return concreteArea * 1000.0 + fenceLength * 2000.0;
+}
