b1aa3a27da7232c32e868bedf0f91d8da5939af0
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-30 02:01:29 +0300

    Update tasks.cpp

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -1,24 +1,30 @@
-// Copyright 2025 UNN-CS Team
+// Copyright 2024 New Developer
 #include "tasks.h"
 #include "circle.h"
 
-double calculateRopeGap() {
-    const double earthRadius = 6378100.0; // В метрах
-    Circle earth(earthRadius);
-    earth.setFerence(earth.getFerence() + 1.0);
-    return earth.getRadius() - earthRadius;
+namespace {
+    constexpr double EARTH_RADIUS_KM = 6378.1;
+    constexpr double ROPE_ADDITION_M = 0.001;
+    constexpr double POOL_RADIUS = 3.0;
+    constexpr double PATH_WIDTH = 1.0;
+    constexpr double CONCRETE_PRICE = 1000.0;
+    constexpr double FENCE_PRICE = 2000.0;
 }
 
+// Задача "Верёвка"
+double calculateGap() {
+    Circle earth(EARTH_RADIUS_KM);
+    earth.setCircumference(earth.getCircumference() + ROPE_ADDITION_M);
+    return earth.getRadius() - EARTH_RADIUS_KM;
+}
+
+// Задача "Бассейн"
 double calculatePoolCost() {
-    const double poolRadius = 3.0;
-    const double width = 1.0;
-    const double outerRadius = poolRadius + width;
-
-    Circle pool(poolRadius);
-    Circle outer(outerRadius);
-
-    double concreteArea = outer.getArea() - pool.getArea();
-    double fenceLength = outer.getFerence();
-
-    return concreteArea * 1000.0 + fenceLength * 2000.0;
+    Circle pool(POOL_RADIUS);
+    Circle poolWithPath(POOL_RADIUS + PATH_WIDTH);
+    
+    double concreteCost = (poolWithPath.getArea() - pool.getArea()) * CONCRETE_PRICE;
+    double fenceCost = poolWithPath.getCircumference() * FENCE_PRICE;
+    
+    return concreteCost + fenceCost;
 }
