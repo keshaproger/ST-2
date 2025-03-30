5c6de6ec4bedb80a1a3b0d015f5bb55274584f87
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-30 04:47:48 +0300

    Update circle.cpp

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,48 +1,42 @@
-// Copyright 2025 Коньков И.
-
+// Copyright 2024 New Developer
 #include "circle.h"
 #include <cmath>
+#include <stdexcept>
 
-const double PI = 3.141592653589793;
-
-Circle::Circle() : radius(0) {
-    updateFromRadius();
+Circle::Circle(double radius) {
+    setRadius(radius);
 }
 
-Circle::Circle(double r) : radius(r) {
-    updateFromRadius();
+void Circle::computeCircumference() {
+    m_circumference = 2 * M_PI * m_radius;
 }
 
-void Circle::updateFromRadius() {
-    ference = 2 * PI * radius;
-    area = PI * radius * radius;
+void Circle::computeArea() {
+    m_area = M_PI * m_radius * m_radius;
 }
 
-void Circle::updateFromFerence() {
-    radius = ference / (2 * PI);
-    area = PI * radius * radius;
+void Circle::setRadius(double radius) {
+    if (radius < 0) throw std::invalid_argument("Radius must be non-negative");
+    m_radius = radius;
+    computeCircumference();
+    computeArea();
 }
 
-void Circle::updateFromArea() {
-    radius = sqrt(area / PI);
-    ference = 2 * PI * radius;
+void Circle::setCircumference(double circumference) {
+    if (circumference < 0)
+        throw std::invalid_argument("Circumference must be non-negative");
+    m_circumference = circumference;
+    m_radius = circumference / (2 * M_PI);
+    computeArea();
 }
 
-void Circle::setRadius(double r) {
-    radius = r;
-    updateFromRadius();
+void Circle::setArea(double area) {
+    if (area < 0) throw std::invalid_argument("Area must be non-negative");
+    m_area = area;
+    m_radius = sqrt(area / M_PI);
+    computeCircumference();
 }
 
-void Circle::setFerence(double f) {
-    ference = f;
-    updateFromFerence();
-}
-
-void Circle::setArea(double a) {
-    area = a;
-    updateFromArea();
-}
-
-double Circle::getRadius() const { return radius; }
-double Circle::getFerence() const { return ference; }
-double Circle::getArea() const { return area; }
+double Circle::radius() const { return m_radius; }
+double Circle::circumference() const { return m_circumference; }
+double Circle::area() const { return m_area; }
