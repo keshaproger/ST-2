52c85859c631a7caaa7be4c9821a50db2f759901
keshaproger <125856206+keshaproger@users.noreply.github.com>
2025-03-30 04:47:07 +0300

    Update circle.h

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,26 @@
-// Copyright 2022 UNN-CS
+// Copyright 2024 New Developer
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
-#include <cstdint>
 
+class Circle {
+ private:
+    double m_radius;
+    double m_circumference;
+    double m_area;
 
-#endif  // INCLUDE_CIRCLE_H_
+    void computeCircumference();
+    void computeArea();
+
+ public:
+    explicit Circle(double radius);
+
+    void setRadius(double radius);
+    void setCircumference(double circumference);
+    void setArea(double area);
+
+    double radius() const;
+    double circumference() const;
+    double area() const;
+};
+
+#endif // INCLUDE_CIRCLE_H_
